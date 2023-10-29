e0ed4a902f8074fee508318b3b40c15610f5382c
ChakiB63 <cchhaakkiibb63@gmail.com>
2023-10-29 20:07:04 +0100

    Interface de connexion Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'application.dart';
 
-import 'package:tp3/tasks.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(elevation: 20),
+        useMaterial3: true,
+      ),
+      home: const MyApp(),
+    ),
+  );
+}
+
+/*
 void main() {
   runApp(
     MaterialApp(
@@ -13,3 +32,4 @@ void main() {
     ),
   );
 }
+*/
